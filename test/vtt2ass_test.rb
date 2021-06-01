d38017d949c8027846ff97c48597dd4670698a25
Louis-Philippe Fortin <timemaster.lpf@gmail.com>
2021-05-31 20:01:15 -0400

    rework of unit testing (wip)

--- test/vtt2ass_test.rb
+++ test/vtt2ass_test.rb
@@ -13,42 +13,6 @@ class Vtt2assTest < Minitest::Test
     refute_nil ::Vtt2ass::VERSION
   end
 
-  def test_that_vtt_file_is_not_nil
-    refute_nil @vtt_file
-  end
-
-  def test_that_vtt_file_has_lines
-    refute_nil @vtt_file.lines
-  end
-
-  def test_that_vtt_file_is_instance_of_VTTFile
-    assert_instance_of VTTFile, @vtt_file
-  end
-
-  def test_that_vtt_file_has_at_least_one_line
-    assert @vtt_file.lines.count >= 1
-  end
-
-  def test_that_vtt_line_is_instance_of_VTTLine
-    assert_instance_of VTTLine, @vtt_file.lines[8]
-  end
-
-  def test_vtt_line_time_start_format
-    assert_match (/([\d:]*)\.?(\d*)/), @vtt_file.lines[22].time_start
-  end
-
-  def test_vtt_line_time_end_format
-    assert_match (/([\d:]*)\.?(\d*)/), @vtt_file.lines[22].time_end
-  end
-
-  def test_vtt_line_style_not_empty
-    refute_empty @vtt_file.lines[12].style
-  end
-
-  def test_vtt_line_text_not_empty
-    refute_empty @vtt_file.lines[88].text
-  end
-
   def test_ass_file_creation_not_nil
     refute_nil @ass_file
   end
---
+++ test/vttfile_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class VTTFileTest < Minitest::Test
+   
+    def setup
+        file_path = __dir__ + '/fixtures/example.vtt'
+        @vtt_file = VTTFile.new(file_path, 1920, 1080)
+    end
+
+    def test_that_vtt_file_is_not_nil
+        refute_nil @vtt_file
+    end
+    
+    def test_that_vtt_file_has_lines
+        refute_nil @vtt_file.lines
+    end
+    
+    def test_that_vtt_file_is_instance_of_VTTFile
+        assert_instance_of VTTFile, @vtt_file
+    end
+    
+    def test_that_vtt_file_has_at_least_one_line
+        assert @vtt_file.lines.count >= 1
+    end
+
+end
