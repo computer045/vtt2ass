30046d48378be6a704d31e486963cfa716fc348c
Louis-Philippe Fortin <timemaster.lpf@gmail.com>
2021-11-21 14:46:35 -0500

    break the tests to match classes

---
+++ test/assfile_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class ASSFileTest < Minitest::Test
+
+    def setup
+        file_path = __dir__ + '/fixtures/example.vtt'
+        vtt_file = VTTFile.new(file_path, 1920, 1080)
+        @ass_file = ASSFile.new(File.basename(file_path).gsub('.vtt', ''), 1920, 1080)
+        @ass_file.convertVTTtoASS(vtt_file, 'Open Sans Semibold', 52)
+    end
+
+    def test_ass_file_creation_not_nil
+        refute_nil @ass_file
+    end
+    
+    def test_ass_file_styles_not_empty
+        refute_empty @ass_file.ass_styles
+    end
+    
+    def test_ass_file_lines_not_empty
+        refute_empty @ass_file.ass_lines
+    end
+    
+end
