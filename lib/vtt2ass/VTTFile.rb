38e7b1715f92a0f95c99a35f8a9af876634279e9
Louis-Philippe Fortin <timemaster.lpf@gmail.com>
2021-11-22 21:52:46 -0500

    dont assign special style name to default center alignment

--- lib/vtt2ass/VTTFile.rb
+++ lib/vtt2ass/VTTFile.rb
@@ -18,7 +18,10 @@ class VTTFile
             paragraph = paragraph.rstrip.gsub(/[\r\n]/, "\n")
             if not paragraph.eql? "" then
                 vtt_line = VTTLine.new(paragraph, width, height)
-                if vtt_line.style.eql? 'Main' and not vtt_line.params.to_s.empty? then
+                if vtt_line.style.eql? 'Main' and
+                    not vtt_line.params.to_s.empty? and
+                    (not vtt_line.params.to_s.eql? 'align:middle' and
+                    not vtt_line.params.to_s.eql? 'align:center') then
                     vtt_line.style = "Style#{style_count}"
                     style_count += 1
                 end
